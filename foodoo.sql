38fac7b731dbf5d2b3c472c57e88a117918b5cd9
Prathyusha Mattam <prathyusha.mattam@gmail.com>
2018-05-01 15:38:13 -0400

    Adding DB Schema

---
+++ foodoo.sql
@@ -0,0 +1,49 @@
+DROP DATABASE IF EXISTS foodoo;
+CREATE DATABASE foodoo;
+
+\c foodoo;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR (100) UNIQUE NOT NULL,
+    password VARCHAR (100) NOT NULL,
+    location VARCHAR (150),
+    email VARCHAR(100) UNIQUE NOT NULL
+);
+
+CREATE TABLE clients (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR (100) UNIQUE NOT NULL,
+  phonenumber VARCHAR (13),
+  address VARCHAR (150) NOT NULL,
+  email VARCHAR(100) UNIQUE NOT NULL
+);
+
+/*
+menu and shopping list can be json. So, the cusom data (which is not limited just few items) can be saved based on the event.
+for example, menu can be saved as '{
+        "appetizer": "Crabcakes",
+        "main": "Pasta",
+        "dessert": "Bread Pudding",
+        "drink": "Moscow Mule Mocktail"
+      }'
+same thing for shoppinglist as well:
+*/
+CREATE TABLE events (
+  id SERIAL PRIMARY KEY,
+  userId INTEGER NOT NULL,
+  name VARCHAR (150) UNIQUE NOT NULL,
+  eventdate TIMESTAMP,
+  description VARCHAR (150),
+  capacty INTEGER,
+  type VARCHAR (50),
+  client VARCHAR (100),
+  menu JSON,
+  shoppingList JSON
+);
+
+-- insert users for initial testing. An entry will be created when new user is registered
+/*
+INSERT INTO users (username, password, email, location) VALUES 
+  ('prathyusha', '$2a$10$TENai6QG7BWzwA.gqAwdI.P40Zct1IYWHs1ylV4A40pbg1fliQxrS', 'prathyusha@gmail.com', 'Atlanta, GA');
+*/
